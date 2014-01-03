7614334a798b6194ee90ad54c723662c38e5cacd
Paul92 <paul.gafton@gmail.com>
2014-01-04 01:24:57 +0200

    Added team module

---
+++ src/team.h
@@ -0,0 +1,63 @@
+/**
+ * @file player.h
+ * @brief Player structure, with helper functions.
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "deck.h"
+
+/*
+ * @brief The maximum number of cards in a player's hand.
+ */
+#define MAX_CARDS 8
+
+/*
+ * @brief The maximum number of players in a team.
+ */
+#define MAX_PLAYERS 2
+
+/**
+ * @brief Player structure.
+ */
+struct Player{
+    int id;
+    char *name;
+    struct Card *hand[MAX_CARDS];
+    int score;
+    int sockfd;
+    int isHuman; //0 for AI, non-zero for human.
+};
+
+/**
+ * @brief Team structure.
+ */
+struct Team{
+    int id;
+    char *name;
+    struct Player *players[MAX_PLAYERS];
+};
+
+/**
+ * @brief Function for basic player creation.
+ *
+ * @param name The name of the new player.
+ * @param sockfd Socket file descriptor for connection.
+ * @param isHuman Player type.
+ *
+ * @return Pointer to the created player. Needs to be freed.
+ */
+struct Player *createPlayer(char *name, int sockfd, int isHuman);
+
+/**
+ * @brief Function for basic team creation.
+ *
+ * @param name The name of the new team.
+ *
+ * @return Pointer to the created team. Needs to be freed.
+ */
+struct Team *createTeam(char *name);
+
+
+#endif

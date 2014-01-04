f63d806a79e890872030a3eeae425ddede2f329c
Paul92 <paul.gafton@gmail.com>
2014-01-04 20:29:31 +0200

    Added Round and Hand structures

---
+++ src/round.h
@@ -0,0 +1,40 @@
+/**
+ * @file round.h
+ * @brief Round and Hand structures, as well as helper functions.
+ */
+
+#ifndef ROUND_H
+#define ROUND_H
+
+#define MAX_HANDS 8
+#define MAX_GAME_PLAYERS 4
+
+#include "deck.h"
+#include "team.h"
+
+/**
+ * @brief Hand structure.
+ *
+ * In a hand, player[i] gives cards[i] and bids bid[i].
+ * The players array should use NULL as terminator.
+ * The players should be added in the order of the bids.
+ */
+struct Hand{
+    struct Card *cards[MAX_GAME_PLAYERS + 1];
+    struct Player *players[MAX_GAME_PLAYERS + 1];
+    int bids[MAX_GAME_PLAYERS];
+};
+
+/**
+ * @brief Round structure.
+ * 
+ * hands uses NULL as terminator.
+ */
+struct Round{
+    int id;
+    enum Suit trump;
+    struct Hand *hands[MAX_HANDS + 1];
+};
+
+#endif
+

4632b87460ddd95af35adfcfd73c7e777a6c4c19
Daniel Jurcau <contact@danijurcau.com>
2014-01-19 10:41:34 +0200

    Fixed #64

--- test/run-test.sh
+++ test/run-test.sh
@@ -11,4 +11,21 @@ if test -z "$CUTTER"; then
     CUTTER="`make -s -C $BASE_DIR echo-cutter`"
 fi
 
+case `uname` in
+    CYGWIN*)
+        PATH="$top_dir/src/.libs:$PATH"
+        ;;
+    Darwin)
+        DYLD_LIBRARY_PATH="$top_dir/src/.libs:$DYLD_LIBRARY_PATH"
+        export DYLD_LIBRARY_PATH
+        ;;
+    *BSD)
+        LD_LIBRARY_PATH="$top_dir/src.libs:$LD_LIBRARY_PATH"
+        export LD_LIBRARY_PATH
+        ;;
+    *)
+        :
+        ;;
+esac
+
 $CUTTER -s $BASE_DIR "$@" $BASE_DIR

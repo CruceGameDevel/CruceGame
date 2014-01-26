df8a7e5fb60b7bcab2000965564eda82d72d0d95
Alin Enachescu <alinenachescu033@gmail.com>
2014-01-26 15:22:21 +0200

    Added the function for adding a player in a team

--- src/team.h
+++ src/team.h
@@ -59,5 +59,15 @@ struct Player *team_createPlayer(char *name, int sockfd, int isHuman);
  */
 struct Team *team_createTeam(char *name);
 
+/**
+* @brief Function for adding a player in a team.
+*
+* @param team The team in that is added the player.
+* @param player The player that to be added in the team.
+*
+* @return 0 If the player was added in team.
+* @return -1 If the player not was added in team.
+*/
+int team_addPlayer(struct Team *team, struct Player *player)
 
 #endif

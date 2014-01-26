f785b0beaceaabd15dd84eb0c62a4464869f4b84
U-Acer-PC\Acer <Acer@Acer-PC.(none)>
2014-01-26 22:31:50 +0200

    Added: tests for function addPlayer

--- test/test-team.c
+++ test/test-team.c
@@ -40,3 +40,30 @@ void test_team_createTeam()
         cut_assert_not_equal_int(check[i], 0);
 }
 
+void test_team_addPlayer()
+{
+    struct Player *player1 = NULL;
+    struct Player *player2 = NULL;
+    struct Player *player3 = NULL;
+    struct Team *team1     = NULL;
+    
+    cut_assert_equal_int(-1, team_addPlayer(team1, player1));
+    
+   /** team1 = team_createTeam("E");
+    cut_assert_equal_int(-1, team_addPlayer(team1, player1));
+    
+    team1 = NULL;
+    player1 = team_createPlayer("A", 0, 0);
+    cut_assert_equal_int(-1, team_addPlayer(team1, player1));
+    
+    team1 = team_createTeam("E");
+    player1 = team_createPlayer("A", 0, 0);
+    cut_assert_equal_int(0, team_addPlayer(team1, player1));
+    cut_assert_equal_int(-1, team_addPlayer(team1, player1));
+    
+    player2 = team_createPlayer("B", 0, 0);
+    player3 = team_createPlayer("C", 0, 0);
+    cut_assert_equal_int(0, team_addPlayer(team1, player2));
+    cut_assert_equal_int(-1, team_addPlayer(team1, player3));**/
+    
+}

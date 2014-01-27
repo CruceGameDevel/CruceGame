66a9b35ad165f12f99c4676099bf3e71c5452438
Paul92 <paul.gafton@gmail.com>
2014-01-27 17:27:17 +0200

    Used deck_newDeck() and deck_deleteDeck() in all tests

--- test/test-deck.c
+++ test/test-deck.c
@@ -1,17 +1,6 @@
 #include <deck.h>
 #include <cutter.h>
-
-static struct Deck deck;
-
-void cut_setup()
-{
-    deck_deckInit(&deck);
-}
-
-void test_deck_test_deckInit()
-{
-    cut_assert_equal_int(0, deck_deckInit(&deck));
-}
+#include <string.h>
 
 int cardsEqual(struct Card a, struct Card b)
 {
@@ -31,29 +20,38 @@ void test_deck_deleteDeck()
 
 void test_deck_test_NewDeck()
 {
+    struct Deck *deck = deck_newDeck();
+
     int duplicates = 0;
     for (int i = 0; i < DECK_SIZE; i++) {
         for (int j = 0; j < DECK_SIZE; j++) {
             if (i != j) {
-                duplicates += cardsEqual(deck.cards[i], deck.cards[j]);
+                duplicates += cardsEqual(deck->cards[i], deck->cards[j]);
             }
         }
     }
+    deck_deleteDeck(&deck);
 }
 
 void test_deck_test_DeckShuffle()
 {
-    struct Deck shuffled = deck;
-    cut_assert_equal_int(0, deck_deckShuffle(&shuffled));
+    struct Deck *deck = deck_newDeck();
+    struct Deck *shuffled = malloc(sizeof(struct Deck));
+    memcpy(shuffled, deck, sizeof(struct Deck));
+
+    cut_assert_equal_int(0, deck_deckShuffle(shuffled));
 
     int differences = 0;
     for (int i = 0; i < DECK_SIZE; i++) {
-        if (!cardsEqual(shuffled.cards[i], deck.cards[i])) {
+        if (!cardsEqual(shuffled->cards[i], deck->cards[i])) {
             differences++;
         }
     }
 
     cut_assert_not_equal_int(differences, 0);
+
+    deck_deleteDeck(&deck);
+    deck_deleteDeck(&shuffled);
 }
 
 void test_deck_test_extensive_DeckShuffle()

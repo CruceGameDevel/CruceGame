31429da8b6b70e5f4e29ab123c086478ef0ad795
Paul92 <paul.gafton@gmail.com>
2014-01-27 15:34:42 +0200

    Fixed compile error

--- test/test-team.c
+++ test/test-team.c
@@ -54,10 +54,10 @@ void test_team_addPlayer()
     cut_assert_equal_int(TEAM_FULL, team_addPlayer(team1, player1));
     free(player1);
     player1 = NULL;
-    
+
     free(team1);
     team1 = NULL;
-    
+
     for (int i = 0; i < MAX_PLAYERS; i++) {
         free(testPlayer[i]);
         testPlayer[i] = NULL;
@@ -67,35 +67,36 @@ void test_team_addPlayer()
 void test_team_removePlayer()
 {
     cut_assert_not_equal_int(NO_ERROR, team_removePlayer(NULL, NULL));
-    
+
     struct Team *team1 = team_createTeam("A"); 
     struct Player *testPlayer[MAX_PLAYERS];
     for (int i = 0; i < MAX_PLAYERS; i++) {
         testPlayer[i] = team_createPlayer("A", i, i);
-        team_addPlayer(team, testPlayer[i]);
+        team_addPlayer(team1, testPlayer[i]);
     }
-    
+
     cut_assert_equal_int(PLAYER_NULL, team_removePlayer(team1, NULL));
     cut_assert_equal_int(TEAM_NULL, team_removePlayer(NULL, testPlayer[0]));
-    
+
     for (int i = 0; i < MAX_PLAYERS; i++) {
         cut_assert_equal_int(NO_ERROR, team_removePlayer(team1, testPlayer[i]));
 
         int removed = 1;
         for (int j = 0; j < MAX_PLAYERS; j++) {
-            if (team->players[j] == testPlayer[i])
+            if (team1->players[j] == testPlayer[i])
                 removed = 0;
         }
         cut_assert_equal_int(1, removed);
 
-        cut_assert_equal_int(NOT_FOUND, team_removePlayer(team1, testPlayer[i]));
+        cut_assert_equal_int(NOT_FOUND,
+                             team_removePlayer(team1, testPlayer[i]));
     }
 
     for (int i = 0; i < MAX_PLAYERS; i++) {
         free(testPlayer[i]);
         testPlayer[i] = NULL;
     }
-    
+
     free(team1);
     team1 = NULL;
 }

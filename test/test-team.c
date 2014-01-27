c6d9ce80d24f59ac5640319d75a1f3327eed3f45
EnachescuAlin <alinenachescu033@gmail.com>
2014-01-27 13:32:45 +0200

    Rethinking tests for the functions addPlayer and removePlayer.

--- test/test-team.c
+++ test/test-team.c
@@ -31,58 +31,48 @@ void test_team_addPlayer()
     cut_assert_equal_int(-1, team_addPlayer(NULL, NULL));
     
     struct Team *team1 = team_createTeam("E");
-    cut_assert_equal_int(-1, team_addPlayer(team1, NULL));
-    
-    struct Player *player1 = team_createPlayer("A", 0, 0);
-    cut_assert_equal_int(-1, team_addPlayer(NULL, player1));
-    
     struct Player *testPlayer[MAX_PLAYERS];
-    for (int i = 0; i < MAX_PLAYERS; i++)
-        testPlayer[i] = team_createPlayer("A", i, i);
-    
     for (int i = 0; i < MAX_PLAYERS; i++) {
+        testPlayer[i] = team_createPlayer("A", i, i);
         cut_assert_equal_int(0, team_addPlayer(team1, testPlayer[i]));
         cut_assert_equal_int(-1, team_addPlayer(team1, testPlayer[i]));
+        cut_assert_equal_int(-1, team_addPlayer(NULL, testPlayer[i]));
+        cut_assert_equal_int(-1, team_addPlayer(team1, NULL));
         int playerAdded = -1; //0 the player was added. Otherwise -1.
         for (int j = 0; j < MAX_PLAYERS; j++)
             if (team1->players[j] == testPlayer[i])
                 playerAdded = 0;
         cut_assert_equal_int(0, playerAdded);
     }
-        
-    cut_assert_equal_int(-1, team_addPlayer(team1, testPlayer[0]));
+    
+    struct Player *player1 = team_createPlayer("A", 1, 1);
+    cut_assert_equal_int(-1, team_addPlayer(team1, player1));
+    free(player1);
+    player1 = NULL;
+    
+    free(team1);
+    team1 = NULL;
     
     for (int i = 0; i < MAX_PLAYERS; i++) {
         free(testPlayer[i]);
         testPlayer[i] = NULL;
     }
-    
-    free(team1);
-    team1 = NULL;
-    free(player1);
-    player1 = NULL;
 }
 
 void test_team_removePlayer()
 {
     cut_assert_equal_int(-1, team_removePlayer(NULL, NULL));
     
-    struct Team *team1 = team_createTeam("A");
-    cut_assert_equal_int(-1, team_removePlayer(team1, NULL));
-    
-    struct Player *player1 = team_createPlayer("P", 1, 1);
-    cut_assert_equal_int(-1, team_removePlayer(NULL, player1));
-    cut_assert_equal_int(-1, team_removePlayer(team1, player1));
-    free(player1);
-    player1 = NULL;
-    
+    struct Team *team1 = team_createTeam("A"); 
     struct Player *testPlayer[MAX_PLAYERS];
     for (int i = 0; i < MAX_PLAYERS; i++) {
         testPlayer[i] = team_createPlayer("A", i, i);
         cut_assert_equal_int(0, team_addPlayer(team1, testPlayer[i]));
         cut_assert_equal_int(0, team_removePlayer(team1, testPlayer[i]));
-        free(testPlayer[i]);
-        testPlayer[i] = NULL;
+        cut_assert_equal_pointer(team1->players[0], NULL);   
+        cut_assert_equal_int(-1, team_removePlayer(NULL, testPlayer[i]));
+        cut_assert_equal_int(-1, team_removePlayer(team1, testPlayer[i]));
+        cut_assert_equal_int(-1, team_removePlayer(team1, NULL));
     }
     
     free(team1);

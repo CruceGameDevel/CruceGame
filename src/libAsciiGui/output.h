f465e981cd2145978b4010d2d5b8a5fd72ba355b
Paul92 <paul.gafton@gmail.com>
2014-02-02 16:18:09 +0200

    Added documentation for printCards() and printPlayerCards()

--- src/libAsciiGui/output.h
+++ src/libAsciiGui/output.h
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include <libCruceGame.h>
 
+/**
+ * @brief Function to print a Card.
+ *
+ * @param card Pointer to the card to be printed.
+ * @param position Position of the card on screen (in cards).
+ *
+ * @return NO_ERROR or 0 on success, other value on failure.
+ */
 int printCard(struct Card *card, int position);
+
+/**
+ * @brief Function to print all cards in a player's hand.
+ *
+ * @param player The player whose cards to be printed.
+ *
+ * @return NO_ERROR or 0 on success, other value on failure.
+ */
 int printPlayerCards(struct Player *player);

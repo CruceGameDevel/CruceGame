d58eff910512ff70a11198dc3037a70630923f3e
EnachescuAlin <alinenachescu033@gmail.com>
2014-02-03 22:43:44 +0200

    Added a new module and the functions: createGame, deleteGame, addPlayer, removePlayer, removeTeam

---
+++ src/libCruceGame/game.c
@@ -0,0 +1,96 @@
+#include "game.h"
+#include "errors.h"
+#include "constants.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Game *game_createGame()
+{
+    struct Game *newGame = malloc(sizeof(struct Game));
+    if (newGame == NULL)
+        return NULL;
+
+    for (int i = 0; i < MAX_GAME_PLAYERS; i++)
+        newGame->players[i] = NULL;
+
+    for (int i = 0; i < MAX_GAME_TEAMS; i++)
+        newGame->teams[i] = NULL;
+
+    newGame->numberPlayers = 0;
+    newGame->round = NULL;
+    newGame->deck = NULL;
+
+    return newGame;
+}
+
+int game_deleteGame(struct Game **game)
+{
+    if (game == NULL)
+        return POINTER_NULL;
+    if (*game == NULL)
+        return GAME_NULL;
+
+    free(*game);
+    *game = NULL;
+
+    return NO_ERROR;
+}
+
+int game_addPlayer(struct Player *player, struct Game *game)
+{
+    if (player == NULL)
+        return PLAYER_NULL;
+    if (game == NULL)
+        return GAME_NULL;
+
+    for (int i = 0; i < MAX_GAME_PLAYERS; i++) {
+        if (game->players[i] == NULL) {
+            game->players[i] = player;
+            return NO_ERROR;
+        }
+        if (game->players[i] == player)
+            return DUPLICATE;
+    }
+
+    return FULL;
+}
+
+int game_removePlayer(struct Player *player, struct Game *game)
+{
+    if (player == NULL)
+        return PLAYER_NULL;
+    if (game == NULL)
+        return GAME_NULL;
+
+    int i = 0;
+    while (i < MAX_GAME_PLAYERS && game->players[i] != player)
+        i++;
+
+    if (i == MAX_GAME_PLAYERS)
+        return NOT_FOUND;
+
+    game->players[i] = NULL;
+
+    return NO_ERROR;
+}
+
+int game_removeTeam(struct Team *team, struct Game *game)
+{
+    if (team == NULL)
+        return TEAM_NULL;
+    if (game == NULL)
+        return GAME_NULL;
+
+    int i = 0;
+    while (i < MAX_GAME_TEAMS && game->teams[i] != team)
+        i++;
+
+    if (i == MAX_GAME_TEAMS)
+        return NOT_FOUND;
+
+    game->teams[i] = NULL;
+
+    return NO_ERROR;
+}
+

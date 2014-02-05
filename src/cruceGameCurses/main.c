c0f93629917ed4030175a5658a2d73195a6a3ab4
Paul92 <paul.gafton@gmail.com>
2014-02-05 16:13:09 +0200

    Added player creation logic

--- src/cruceGameCurses/main.c
+++ src/cruceGameCurses/main.c
@@ -13,10 +13,14 @@ int main()
 
     struct Deck *deck = deck_createDeck();
     deck_deckShuffle(deck);
-    struct Player *player = team_createPlayer("FirstPlayer", 0, 0);
-    team_addCard(player, deck->cards[0]);
 
-    printPlayerCards(player);
+    struct Game *game = game_createGame();
+    for (int i = 0; i < noOfPlayers; i++) {
+        int err = game_addPlayer(newPlayer(i), game);
+        if (err != 0)
+            printw("ERROR: game_addPlayer() %d\n", err);
+    }
+
 
     getch();
     endwin();

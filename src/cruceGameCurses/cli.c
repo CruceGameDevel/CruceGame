8b3900d47e9a68d6bea6fdbf81ef47a8cfa0c46a
EnachescuAlin <alinenachescu033@gmail.com>
2014-03-08 14:27:49 +0200

    Added displayCardsAndPickCard()

--- src/cruceGameCurses/cli.c
+++ src/cruceGameCurses/cli.c
@@ -283,4 +283,68 @@ int formTeams (struct Game *game)
     return NO_ERROR;
 }
 
+int displayCardsAndPickCard(struct Game *game, int playerId)
+{
+    if (game == NULL)
+        return GAME_NULL;
+    if (playerId < 0 || playerId >= MAX_GAME_PLAYERS)
+        return ILLEGAL_VALUE;
+    if (game->round == NULL)
+        return ROUND_NULL;
+    if (game->round->players[playerId] == NULL)
+        return PLAYER_NULL;
+
+    int handId;
+    for (int i = 0; i < MAX_HANDS; i++)
+        if (game->round->hands[i] != NULL)
+            handId = i;
+    if (game->round->hands[handId] == NULL)
+        return HAND_NULL;
+
+    char suit[] = {0xE2, 0x99, 0x00, 0x00};
+    switch (game->round->trump) {
+        case DIAMONDS:
+            suit[2] = 0xA6;
+            break;
+        case CLUBS:
+            suit[2] = 0xA3;
+            break;
+        case HEARTS:
+            suit[2] = 0xA5;
+            break;
+        case SPADES:
+            suit[2] = 0xA0;
+            break;
+        default:
+            break;
+    }
+
+    printw("Player %d %s\n", playerId + 1, 
+                             game->round->players[playerId]->name);    
+
+    if (game->round->trump != SuitEnd)
+        printw("The trump is: %s\n", suit);
+    else
+        printw("The trump was not set.\n");
+
+    printw("The cards on table: ");
+    for (int i = 0; i < MAX_GAME_PLAYERS; i++)
+        if (game->round->hands[handId]->players[i] != NULL)
+            printCard(game->round->hands[handId]->cards[i], i);
+
+    int y, x;
+    getyx(stdscr, y, x);
+    move(y + 7, 0);
+    printw("Your cards: ");
+    printPlayerCards(game->round->players[playerId]);
+
+    move(y + 14, 0);
+    int idCard = pickCard(game->round->players[playerId],
+                          game, game->round->hands[handId]);
+    round_putCard(game->round->players[playerId], idCard,
+                  game->round->hands[handId]);
+
+    return NO_ERROR;
+}
+
 

e91f7d6b970a53927b2624a2f173bd4440da334b
YoNuTzZ <ionut.grt@gmail.com>
2014-04-01 20:36:32 +0300

    Added duplicates name checking

--- src/cruceGameCurses/main.c
+++ src/cruceGameCurses/main.c
@@ -31,7 +31,10 @@ int main()
 
     struct Game *game = game_createGame(limitScore);
     for (int i = 0; i < noOfPlayers; i++) {
-        int err = game_addPlayer(newPlayer(i + 1), game);
+        int err;
+        while ((err = game_addPlayer(newPlayer(i + 1), game)) == DUPLICATE_NAME)
+             printw("The player's name have to be unique\n");
+        err = game_addPlayer(newPlayer(i + 1), game);
         if (err != 0)
             printw("ERROR: game_addPlayer() %d\n", err);
     }

da371c3b17004fdf8f862c1eb15d894a06ca0c1d
Paul92 <paul.gafton@gmail.com>
2014-04-08 21:38:24 +0300

    Colored suits differently and marked only card number with red for forbidden cards

--- src/cruceGameCurses/main.c
+++ src/cruceGameCurses/main.c
@@ -30,6 +30,10 @@ int main()
 
     start_color();
     init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_BLUE, COLOR_BLACK);
+    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
     refresh();
 
     welcomeMessage();

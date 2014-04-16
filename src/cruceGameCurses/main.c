7d27c80067989bdd3880daec04f4a25ea6f9a7c2
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-16 14:54:16 +0300

    Added a new color.

--- src/cruceGameCurses/main.c
+++ src/cruceGameCurses/main.c
@@ -34,6 +34,7 @@ int main()
     init_pair(3, COLOR_YELLOW, COLOR_BLACK);
     init_pair(4, COLOR_BLUE, COLOR_BLACK);
     init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(7, COLOR_WHITE, COLOR_BLACK);
     refresh();
 
     welcomeMessage();

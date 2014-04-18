084ae20ffb4f3ab8217c4b8cd29ad547e661e011
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-18 16:20:23 +0300

    Added a new constant, SLEEP_TIME.

--- src/cruceGameCurses/cli.c
+++ src/cruceGameCurses/cli.c
@@ -15,6 +15,7 @@
 #define MAX_CARDS_PER_LINE 8
 #define MAX_NAME_SIZE 20
 #define ROUND_DIALOG_SCORE_SIZE 5
+#define SLEEP_TIME 2
 
 #define HIGHLIGHT_ATTRIBUTE A_BLINK
 
@@ -508,7 +509,7 @@ int displayCardsAndPickCard(struct Game *game, int playerId)
         wclear(cardsInHandWindow);
         printPlayerCards(game, player, selected, cardsInHandWindow);
         wrefresh(cardsInHandWindow);
-        sleep(2);
+        sleep(SLEEP_TIME);
     }
 
     delwin(cardsOnTableWindow);

7f19452f6ea95e7d1e76719832f933454db68a83
Paul92 <paul.gafton@gmail.com>
2015-08-24 00:36:53 +0100

    Added network module stub

---
+++ src/network/network.c
@@ -0,0 +1,20 @@
+/**
+ * @file This file provides the implementation of network module.
+ */
+
+void network_connect(char *hostname, int port)
+{
+}
+
+int network_send(void *data, int size)
+{
+}
+
+int network_read(void *buffer, int size)
+{
+}
+
+void network_disconnect()
+{
+}
+
---
+++ src/network/network.h
@@ -0,0 +1,49 @@
+/**
+ * @file Module that implements the network operations.
+ */
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
+/**
+ * @brief Initialize a network connection.
+ *
+ * @param hostname The hostname to connect to.
+ * @param port The port to use.
+ *
+ * @return none
+ */
+void network_connect(char *hostname, int port);
+
+/**
+ * @brief Send a message over the network.
+ *        No assumption is made about the message's format or content.
+ *
+ * @param data Pointer to the data to be sent.
+ * @param size The number of bytes to be sent.
+ *
+ * @return none
+ */
+int network_send(void *data, int size);
+
+/**
+ * @brief Read a message from the network.
+ *
+ * @param buffer Pointer to a memory region where the message read is going to
+ *               be stored. The content of this memory region may be
+ *               overwritten.
+ * @param size The maximum number of bytes to read.
+ *
+ * @return The actual number of bytes read.
+ */
+int network_read(void *buffer, int size);
+
+/**
+ * @brief End a network connection.
+ *
+ * @return none
+ */
+void network_disconnect();
+
+#endif // end of include guard: NETWORK_H
+

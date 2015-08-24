c9ac3146a0dea8f940f5cc4995002497e8aa18a9
Paul92 <paul.gafton@gmail.com>
2015-08-24 21:39:10 +0100

    Added gameProtocol module

---
+++ src/gameProtocol/actionReceiver.h
@@ -0,0 +1,11 @@
+/**
+ * This file is part of the gameProtocol module, which represents the only way
+ * to communicate with the external world. It provides methods for all game
+ * events, such as bidding, putting a card on the table or talking to another
+ * player.
+ * 
+ * The receiver submodule reads the game commands and user messages from the
+ * irc channel and parses them, taking the required actions.
+ *
+ * This submodule implements a set of events used to notify the user.
+ */
---
+++ src/gameProtocol/actionSender.h
@@ -0,0 +1,9 @@
+/**
+ * This file is part of the gameProtocol module, which represents the only way
+ * to communicate with the external world. It provides methods for all game
+ * events, such as bidding, putting a card on the table or talking to another
+ * player.
+ * 
+ * The sender submodule broadcasts the actions of a player by writing game
+ * commands and user messages to the irc channel.
+ */

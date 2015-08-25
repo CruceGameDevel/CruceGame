8c7c8a549032ded0d8ff5389a0973bad2996c0ef
Paul92 <paul.gafton@gmail.com>
2015-08-25 01:03:51 +0100

    Added test-network stubs

---
+++ test/test-networking/test-network.c
@@ -0,0 +1,19 @@
+#include <cutter.h>
+#include "../../src/networking/network.h"
+
+void test_network_connect() {
+    cut_assert_equal_int(0, 0);
+}
+
+void test_network_disconnect() {
+    cut_assert_equal_int(0, 0);
+}
+
+void test_network_read() {
+    cut_assert_equal_int(0, 0);
+}
+
+void test_network_send() {
+    cut_assert_equal_int(0, 0);
+}
+

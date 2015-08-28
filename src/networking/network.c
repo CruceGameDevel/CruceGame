a14bb08d037be57035fcb98bd871a175886c9c1d
Paul92 <paul.gafton@gmail.com>
2015-08-28 19:25:09 +0100

    Fixed compiler errors

--- src/networking/network.c
+++ src/networking/network.c
@@ -48,7 +48,7 @@ int network_connect(char *hostname, int port)
 int network_send(void *data, size_t size)
 {
     if (data == NULL)
-        return NULL_PARAMETER
+        return NULL_PARAMETER;
 
     if (sockfd < 0)
         return UNINITIALIZED_CONNECTION;
@@ -62,7 +62,7 @@ int network_send(void *data, size_t size)
 int network_read(void *buffer, size_t size)
 {
     if (buffer == NULL)
-        return NULL_PARAMETER
+        return NULL_PARAMETER;
 
     if (sockfd < 0)
         return UNINITIALIZED_CONNECTION;

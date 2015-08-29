5aab9415a2283b2448d593950608420761d19f56
Paul92 <paul.gafton@gmail.com>
2015-08-29 18:30:43 +0100

    Begun the actionSender interface

--- src/gameProtocol/actionSender.h
+++ src/gameProtocol/actionSender.h
@@ -7,3 +7,60 @@
  * The sender submodule broadcasts the actions of a player by writing game
  * commands and user messages to the irc channel.
  */
+
+/**
+ * @brief Function to send a REQUEST_BID game command.
+ *
+ * @param user The user to request the bid from.
+ *
+ * @return NO_ERROR on success, other value on failure.
+ */
+int actionSender_requestBid(char *user);
+
+
+/**
+ * @brief Function to send a bid in reply to a REQUEST_BID command.
+ *
+ * @param bidValue The value to bid.
+ *
+ * @return NO_ERROR on success, other value on failure.
+ */
+int actionSender_sendBid(int bidValue);
+
+/**
+ * @brief Function to request a card from a user.
+ *
+ * @param user The user to request the card from.
+ *
+ * @return NO_ERROR on success, other value on failure.
+ */
+int actionSender_requestCard(char *user);
+
+/**
+ * @brief Function to send a card in reply to a REQUEST_CARD command.
+ *
+ * @param card The card to send.
+ *
+ * @return NO_ERROR on success, other value on failure.
+ */
+int actionSender_sendCard(struct Card *card);
+
+/**
+ * @brief Function to distribute a card. Used by the dealer (host).
+ *
+ * @param recipient The name of the recipient of the card.
+ * @param card The card to send.
+ *
+ * @return NO_ERROR on succes, other value on failure.
+ */
+int actionSender_distributeCard(char *recipient, struct Card *card);
+
+/**
+ * @brief Function to update the score. Used by the dealer (host).
+ *
+ * @param score???
+ *
+ * @return NO_ERROR on success, other value on failure.
+ */
+int actionSender_updateScore();
+

63dfcefa48f3650490f0c04eb10b3f4f128f753c
Paul92 <paul.gafton@gmail.com>
2015-08-29 18:51:28 +0100

    Using better cutter assertions instead of cut_assert_true/false

--- test/test-networking/test-network.c
+++ test/test-networking/test-network.c
@@ -24,22 +24,22 @@ int openLocalhostSocket(int port) {
     setsockopt(serverSockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
     cut_assert_true(serverSockfd >= 0, "Server socket opening failed");
 
-
     struct sockaddr_in serv_addr, cli_addr;
     bzero((char *)&serv_addr, sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
-    int bnd = bind(serverSockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    cut_assert_true(bnd >= 0, "Server bind failed");
+    int bnd = bind(serverSockfd, (struct sockaddr *)&serv_addr,
+                   sizeof(serv_addr));
+    cut_assert_operator_int(bnd, >=, 0, "Server bind failed");
 
     listen(serverSockfd, 5);
     socklen_t clilen = sizeof(cli_addr);
 
     int newsockfd = accept(serverSockfd, (struct sockaddr *)&cli_addr, &clilen);
 
-    cut_assert_true(newsockfd >= 0, "Server accept failed");
+    cut_assert_operator_int(newsockfd, >=, 0, "Server accept failed");
 
     return newsockfd;
 }
@@ -57,10 +57,11 @@ void connectToLocalhostSocket(int port)
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     int optval = 1;
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
-    cut_assert_true(sockfd >= 0, "Could not connect to the server thread");
+    cut_assert_operator_int(sockfd, >=, 0,
+                            "Could not connect to the server thread");
 
     struct hostent *server = gethostbyname("localhost");
-    cut_assert_true(server != NULL, "No such host");
+    cut_assert_not_null(server, "No such host");
 
     struct sockaddr_in serv_addr;
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -70,10 +71,11 @@ void connectToLocalhostSocket(int port)
          server->h_length);
     serv_addr.sin_port = htons(port);
 
-    cut_assert_false(connect(sockfd,
-                             (struct sockaddr *)&serv_addr,
-                             sizeof(serv_addr)) < 0,
-                     "Error connecting to the server process");
+    int conn = connect(sockfd, (struct sockaddr *)&serv_addr,
+                       sizeof(serv_addr));
+
+    cut_assert_operator_int(conn, >=, 0,
+                            "Error connecting to the server process");
 }
 
 /**

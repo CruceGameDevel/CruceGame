25d6f066c964985560baeef1881bdda3f6ac46de
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-30 18:48:55 +0300

    Added actionSender_updateScore().

--- src/gameProtocol/actionSender.c
+++ src/gameProtocol/actionSender.c
@@ -93,3 +93,55 @@ int actionSender_distributeCard(char *recipient, struct Card *card)
 
     return sendMessageRet;
 }
+
+/**
+ * Send "$UPDATE_SCORE <N1> <N2> <S1> , <N3> <N4> <S2>" command to room using
+ * irc_sendRoomMessage() when playing with teams.
+ * Send "$UPDATE_SCORE <N1> <S1> , <N2> <S2> , <N3> <S3> , <N4> <S4>" command
+ * to room using irc_sendRoomMessage() when palying without teams.
+ *
+ * N - represents the player name.
+ * S - represents the team score.
+ */
+int actionSender_updateScore(struct Game *game)
+{
+    if (game == NULL)
+        return NULL_PARAMETER;
+
+    int commandSize = 15;
+    for (int i = 0; i < MAX_GAME_TEAMS; i++) {
+        if (game->teams[i] != NULL) {
+            commandSize += 7;
+            for (int j = 0; j < MAX_TEAM_PLAYERS; j++) {
+                if (game->teams[i]->players[j] != NULL) {
+                    commandSize += strlen(game->teams[i]->players[j]->name);
+                }
+            }
+        }
+    }
+
+    char *command = malloc(commandSize);
+    char score[4];
+
+    sprintf(command, "$UPDATE_SCORE ");
+
+    for (int i = 0; i < MAX_GAME_TEAMS; i++) {
+        if (game->teams[i] != NULL) {
+            for (int j = 0; j < MAX_TEAM_PLAYERS; j++) {
+                if (game->teams[i]->players[j] != NULL) {
+                    strcat(command, game->teams[i]->players[j]->name);
+                    strcat(command, " ");
+                }
+            }
+            sprintf(score, "%d", game->teams[i]->score);
+            strcat(command, score);
+            strcat(command, " , ");
+        }
+    }
+
+    int sendMessageRet = irc_sendRoomMessage(command);
+
+    free(command);
+
+    return sendMessageRet;
+}

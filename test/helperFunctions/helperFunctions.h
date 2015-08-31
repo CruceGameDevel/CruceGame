550ca1ff07d24a6324218d69eb2d7e0985d1fa88
EnachescuAlin <alinenachescu033@gmail.com>
2015-08-31 16:14:48 +0300

    Added the declaration and documentation of the openLocalhostSocket().

--- test/helperFunctions/helperFunctions.h
+++ test/helperFunctions/helperFunctions.h
@@ -1,3 +1,17 @@
 /**
  * This file implements helper functions for tests.
  */
+
+#ifndef HELPERFUNCTIONS_H
+#define HELPERFUNCTIONS_H
+
+/**
+ * @brief Open a local server socket and waits a client to connect.
+ *
+ * @param port The port on which must be created the server.
+ *
+ * @return A sockfd which represents a connection with a client..
+ */
+int openLocalhostSocket(int port);
+
+#endif

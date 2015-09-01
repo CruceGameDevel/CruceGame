73abbd56b80018a2a02b2a881e404b2d94d892d6
Cristi D <cristi.dnla@gmail.com>
2015-09-02 02:21:53 +0300

    Completing proof of concept

--- irc_connect_poc.c
+++ irc_connect_poc.c
@@ -1,23 +1,17 @@
 #include <string.h>
+#include <stdio.h>
 #include <strings.h>
-#include <stdlib.h>
-#include <sys/socket.h>
 #include <netdb.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdio.h>
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
-
 #include <errno.h>
+#include <stdbool.h>
+#include <ctype.h>
+
 #define CONNECTION_IN_USE -1
 #define INVALID_HOSTNAME -2
 #define CONNECTING_ERROR -3
@@ -89,6 +83,7 @@ int network_send(void *data, size_t size)
 
     return NO_ERROR;
 }
+
 int irc_connect(char *name)
 {
     // Connect to IRC server and test for errors.
@@ -121,8 +116,53 @@ int irc_connect(char *name)
     return NO_ERROR;
 }
 
-int main() {
-    printf("RET %d\n", irc_connect("test"));
-    sleep(10);
+int main()
+{
+    int pid = fork();
+
+    if(pid == 0)
+    {
+        printf("irc_connect ready\n");
+        printf("RET %d\n", irc_connect("test"));
+        sleep(10);
+        exit(1);
+    }
+
+    // system("sudo timeout 10 tcpflow -C -i any port 6667");
+    printf("tcpflow ready\n");
+    FILE* fp;
+
+    fp = popen("sudo timeout 10 tcpflow -C -i any port 6667", "r");
+
+    char buffer[3 * 512];
+    int size = 0;
+
+    char prevChar = fgetc(fp);
+    char currentChar = fgetc(fp);
+    bool beingParsed = false;
+    while(currentChar != EOF)
+    {
+        if(!beingParsed && prevChar == '\n' && isupper(currentChar))
+        {
+            beingParsed = true;
+        }
+
+        if(beingParsed)
+        {
+            buffer[size] = currentChar;
+            size++;
+            if(currentChar == '\n')
+                beingParsed = false;
+        }
+
+        prevChar = currentChar;
+        currentChar = fgetc(fp);
+    }
+
+    pclose(fp);
+
+    buffer[size] = '\0';
+    printf("%s\n", buffer);
+    printf("%d\n", size);
     return 0;
 }

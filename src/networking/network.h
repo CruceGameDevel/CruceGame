3c3ec5ae52cbe1324534e09cef83a935cf48e9aa
JustBeYou <mihailferaru2000@gmail.com>
2015-09-13 11:30:09 +0300

    Removed timeout parameter.

--- src/networking/network.h
+++ src/networking/network.h
@@ -84,11 +84,9 @@ int network_disconnect();
 /**
  * @brief Check if there is any incoming data.
  *
- * @param timeout Maximum of seconds to wait for input.
- *
  * @return 1 if there is input data, 0 if not.
  */
-int network_checkForData(int timeout);
+int network_checkForData();
 
 #endif // end of include guard: NETWORK_H
 
